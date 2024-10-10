cb7fd03eb4ca1149fe48e11062b49d6e7ad2a294
Prajakt Madkar <prajakt_madkar@persistent.com>
2024-10-10 17:47:48 +0530

    Adding all files to repo

---
+++ main.tf
@@ -0,0 +1,85 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 3.0" }
+  }
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYDWHTT2ZAQQ424FU"
+  secret_key = "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"
+
+}
+
+terraform {
+  backend "s3" {
+    bucket = "demo-bucket-terraform-state-001"
+    key    = "my-terraform-project"
+    region = "us-east-1"
+  }
+}
+
+
+# --- VPC ---
+
+data "aws_availability_zones" "available" { state = "available" }
+
+locals {
+  azs_count = 2
+  azs_names = data.aws_availability_zones.available.names
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags                 = { Name = "demo-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  count                   = local.azs_count
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = local.azs_names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
+  map_public_ip_on_launch = true
+  tags                    = { Name = "demo-public-${local.azs_names[count.index]}" }
+}
+
+
+# --- Internet Gateway ---
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "demo-igw" }
+}
+
+resource "aws_eip" "main" {
+  count      = local.azs_count
+  depends_on = [aws_internet_gateway.main]
+  tags       = { Name = "demo-eip-${local.azs_names[count.index]}" }
+}
+
+
+# --- Public Route Table ---
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "demo-rt-public" }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.azs_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+
+
